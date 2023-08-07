ec83f5c027dcf925dcd14e8e0e0ea3dab072f0ca
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-08-07 15:35:59 +0600

    create ProductImageManager class

--- src/Utils/Manager/ProductManager.php
+++ src/Utils/Manager/ProductManager.php
@@ -60,6 +60,9 @@ class ProductManager
 
         $productDir = $this->getProductImagesDir($product);
 
-        dd($productDir);
+
+        $productImage = $this->productImageManager->saveImageForProduct($productDir, $tempImageFilename);
+        $product->addProductImage($productImage);
+        return $product;
     }
 }
